b0569299a9c2e51e871402b03e660f6030af054b
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-05-18 20:44:14 +0530

    Added Cucumber tests

--- src/test/java/com/akshat/agileboard/AgileBoardApplicationTests.java
+++ src/test/java/com/akshat/agileboard/AgileBoardApplicationTests.java
@@ -1,7 +1,10 @@
 package com.akshat.agileboard;
 
+import com.akshat.cucumber.Stepdefs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 public class AgileBoardApplicationTests {
